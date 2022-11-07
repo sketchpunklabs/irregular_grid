fdad4129949d4e02dcf9c6f8bb15ae8337b3e1f3
Pedro Sousa <sketchpunk@gmail.com>
2022-11-07 09:32:53 -0500

    update

---
+++ mc/DualContour.js
@@ -0,0 +1,3 @@
+// http://www.boristhebrave.com/2018/04/15/dual-contouring-tutorial/
+// https://github.com/BorisTheBrave/mc-dc/blob/master/dual_contour_3d.py#L57
+// https://github.com/fayolle/dual_contouring/blob/main/src/DualContouring.java
